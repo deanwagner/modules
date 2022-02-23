7605f0d81be389501f54c1377f7bc7dec36971f6
Dean Wagner <me@deanwagner.net>
2022-02-23 13:44:31 -0800

    created boiler plates

---
+++ boilerplates/class.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/**
+ * Blank
+ * @class
+ * @property {string} property - Class Property
+ * @author Dean Wagner <info@deanwagner.net>
+ */
+class Blank {
+
+    // Class Properties
+    property = '';
+
+    /**
+     * Constructor
+     * @constructor
+     * @param {string} parameter - Class Parameter
+     */
+    constructor(parameter) {
+        this.property = parameter;
+    }
+
+    /**
+     * Get Class Property
+     * @returns {string} - Class Property
+     */
+    get property() {
+        return this.property;
+    }
+
+    /**
+     * Set Class Property
+     * @param {string} parameter - Method Parameter
+     */
+    set property(parameter) {
+        this.property = parameter;
+    }
+}
+
+export default Blank;
